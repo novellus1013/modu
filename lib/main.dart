ba85622dc334e2d6d598ef687c771ce24ba6e2a9
novellus1013 <novellus1013@gmail.com>
2024-03-14 23:21:20 +0900

    supabase 연동 및 dotenv로 apiKey 환경 변수 등록

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:modu/features/map/map_screen.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+const supabaseUrl = 'https://gbzqvrdhwwvjegxtvyzk.supabase.co';
+String supabaseKey = String.fromEnvironment(dotenv.get('SUPABASE_KEY'));
+
+Future<void> main() async {
+  //supabase 초기화 하기 전 앱 초기화
+  WidgetsFlutterBinding.ensureInitialized();
+
+  //dotenv 파일에서 변수 가져오기
+  await dotenv.load(fileName: ".env");
+
+  //supabase 적용
+  await Supabase.initialize(url: supabaseUrl, anonKey: supabaseKey);
+
   runApp(const MyApp());
 }
 
